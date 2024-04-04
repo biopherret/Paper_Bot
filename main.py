b008756406887c5e418187a74a0c06bd3f80cbd5
biopherret <pi@raspberrypi>
2024-04-03 22:39:24 -0500

    reset and removed token from main

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from discord_slash import SlashCommand # Importing the newly installed library.
 client = discord.Client(intents=discord.Intents.all())
 slash = SlashCommand(client, sync_commands=True) # Declares slash commands through the client.
 
-secret_token = 'MTIyNTIzMDE2ODU5NjE1NjYxOQ.GJ28VQ.vX9X1aJs8UlPhMRgODAWl45TnhLiTBipuvYO7w'
+discord_token = open("discord_token.txt", "r").read()
 
 @client.event
 async def on_ready():
     print("Ready!")
 
-client.run(secret_token)
+client.run(discord_token)
