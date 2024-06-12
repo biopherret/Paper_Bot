dc59b052634d1af5b984d4d7dbfe0ff6186d475f
biopherret <webster.sarah.a@gmail.com>
2024-06-12 14:44:28 -0700

    fix hyperlinks for truncated titels

--- main.py
+++ main.py
@@ -129,9 +129,10 @@ async def find_papers(user, num_papers):
 
     embed = discord.Embed(title="Papers I Found For You")
     for topic_dict in topics_list:
-        paper_list = [f"[{article_dict['title']}]({article_dict['online_link']})" for article_dict in found_articles if article_dict['topic'] == topic_dict['topic']]
-        truncated_paper_list = [truncate_title(paper) for paper in paper_list]
-        embed.add_field(name=f'{topic_dict["topic"]} (Recent Only: {["No", "Yes"][topic_dict["recent"]]})', value="\n".join(truncated_paper_list), inline=False)
+        truncated_title_list = [truncate_title(article_dict['title']) for article_dict in found_articles if article_dict['topic'] == topic_dict['topic']]
+        links = [article_dict['online_link'] for article_dict in found_articles if article_dict['topic'] == topic_dict['topic']]
+        hyperlinked_papers_list = [f"[{truncated_title_list[i]}]({links[i]})" for i in range(len(truncated_title_list))]
+        embed.add_field(name=f'{topic_dict["topic"]} (Recent Only: {["No", "Yes"][topic_dict["recent"]]})', value="\n".join(hyperlinked_papers_list), inline=False)
     discord_user = await bot.fetch_user(user)
     await discord_user.send(embed = embed)
 
