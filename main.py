536e8139e8ce7213f9dcdcb945cb3eca537071b4
biopherret <webster.sarah.a@gmail.com>
2024-06-18 16:48:00 -0700

    remove troubleshooting lines

--- main.py
+++ main.py
@@ -169,7 +169,7 @@ async def getArticles(topics_list, num_papers, user):
     await write_json(topics_json, "topics.json") #save new articles to json
     return new_articles
 
-async def get_text_for_LM(paper_title, doc_type, doc_link, online_link, user):
+async def get_text_for_LM(paper_title, doc_type, doc_link, online_link):
     if doc_type == 'PDF':  
         try:
             context_text = await read_pdf(doc_link, paper_title)
@@ -229,10 +229,7 @@ async def find_papers(user, num_papers):
         context_txt = await get_text_for_LM(article_dict['title'], article_dict['doc_type'], article_dict['doc_link'], article_dict['online_link'], user)
         if context_txt != None:
             summary_txt = await get_summary_from_LM(context_txt)
-            try:
-                file = await text_to_mp3(summary_txt, article_dict['title'])
-            except:
-                print(summary_txt)
+            file = await text_to_mp3(summary_txt, article_dict['title'])
             await discord_user.send(file=file, content = "")
         await progress_mes.edit(content = "I will now attempt to summarize the papers for you. This may take a while, and I am not always able to summarize every paper.\n {}".format(progressBar.filledBar(num_found, i, size = num_found)[0]))   
     await discord_user.send("Done!")
