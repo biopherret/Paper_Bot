075ae388aa6787a23f64b9a64e77daf9d45fa1f7
biopherret <webster.sarah.a@gmail.com>
2024-06-18 18:28:08 -0700

    small bug fix

--- main.py
+++ main.py
@@ -234,7 +234,7 @@ async def find_papers(user, num_papers):
     progress_mes = await discord_user.send("I will now attempt to summarize the papers for you. This may take a while, and I am not always able to summarize every paper\n {}".format(progressBar.filledBar(num_found, i, size = num_found)[0]))
     for article_dict in found_articles:
         i += 1
-        context_txt = await get_text_for_LM(article_dict['title'], article_dict['doc_type'], article_dict['doc_link'], article_dict['online_link'], user)
+        context_txt = await get_text_for_LM(article_dict['title'], article_dict['doc_type'], article_dict['doc_link'], article_dict['online_link'])
         if context_txt != None:
             summary_txt = await get_summary_from_LM(context_txt)
             file = await text_to_mp3(summary_txt, article_dict['title'])
